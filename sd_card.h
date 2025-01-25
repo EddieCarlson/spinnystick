b965e2f6374932c923773b2455ef9a9632d1ac88
Eddie Carlson <carlson.eddie@gmail.com>
2025-01-24 17:50:25 -0800

    helps to add the sd_card stuff

---
+++ sd_card.h
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <SD.h>
+#include <FastLED.h>
+#include "common.h"
+#include "animation/raydisplay.h"
+
+volatile bool changeAnimations = false;
+int animationIndex = 0;
+
+void listAnimations() {
+  File root = SD.open("/");
+  File f = root.openNextFile();
+  Serial.println(f.name());
+  for(int i = 0; i < animationIndex; i++) {
+    f.close();
+    f = root.openNextFile();
+    Serial.println(f.name());
+    if (!f) {
+      break;
+    }
+  }
+}
+
+uint8_t charToHex(char hexChar) {
+  if (hexChar >= '0' && hexChar <= '9') {
+      return hexChar - '0';
+  } else if (hexChar >= 'A' && hexChar <= 'F') {
+      return hexChar - 'A' + 10;
+  } else if (hexChar >= 'a' && hexChar <= 'f') {
+      return hexChar - 'a' + 10;
+  } else {
+    Serial.print("bad hex-char conversion from: ");
+    Serial.println(hexChar);
+    return 0;
+  }
+}
+
+void importImageFromSD(String name) {
+  File myFile = SD.open(name.c_str());
+  int ray = 0;
+  int px = 0;
+  while (myFile.available()) {
+    if (ray >= NUM_RAYS) {
+      break;
+    }
+    switch ((char)myFile.peek()) {
+      case ',':
+      myFile.read();
+      break;
+      case '|':
+      ray++;
+      px = 0;
+      myFile.read();
+      break;
+
+      default:
+      int ri = myFile.read(); 
+      int gi = myFile.read(); 
+      int bi = myFile.read(); 
+
+      uint8_t r = min(255, charToHex(ri) * 16);
+      uint8_t g = min(255, charToHex(gi) * 16);
+      uint8_t b = min(255, charToHex(bi) * 16);
+
+      rays[ray][px] = CRGB(r, g, b);
+
+      px++;
+    }
+  }
+  myFile.close();
+}
+
+String readFileFromSerial() {
+  delay(1000);
+  uint32_t startMillis = millis();
+  Serial.println("waiting for serial input");
+  while(!Serial.available()) {
+    if (millis() - startMillis < 300000) {
+      delay(1000);
+    } else {
+      Serial.println("serial input received");
+      return;
+    }
+  }
+  String title = Serial.readStringUntil('|', 200);
+  File myFile = SD.open(title.c_str(), FILE_WRITE);
+  while(true) {
+    myFile.write(Serial.read());
+    if (!Serial.available()) {
+      delay(10);
+      if (!Serial.available()) {
+        break;
+      }
+    }
+  }
+  Serial.println("finished writing file");
+  delay(50);
+  myFile.close();
+  delay(20);
+  return title;
+}
