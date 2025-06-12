01b97ba07da9c85364a2593e29ef3659161f96b1
EddieCarlson <carlson.eddie@gmail.com>
2025-06-12 14:17:38 -0700

    cleanup

--- imu_init.h
+++ imu_init.h
@@ -25,7 +25,7 @@ File sensorLog;
 #define smoothingWindow 5
 
 int orientationHistoryPointer = 0;
-int gyroHistoryPointer = 0;
+int historyCurIndex = 0;
 double revTrackingAngle = 0;
 double curAngleEstimate = -1;
 int revTrackingStartPointer = 0;
@@ -35,15 +35,15 @@ bool newUpIndex = false;
 
 double minDegreesPerSec = 800;
 
-double yAccelHistory[gyroHistorySize]; // most recent samples of acceleration up/down
-double gyroZHistory[gyroHistorySize]; // most recent samples degrees per second of revolution
-double smoothedGyroHistory[gyroHistorySize];
-unsigned long timestamps[gyroHistorySize];
+double accelHistory[historySize]; // most recent samples of acceleration up/down
+double gyroHistory[historySize]; // most recent samples degrees per second of revolution
+double smoothedAccelHistory[historySize];
+unsigned long timestamps[historySize];
 
 unsigned long upTimestamps[orientationHistorySize]; // most recent best geusses at when the stick was pointed upwards
 unsigned long downTimestamps[orientationHistorySize]; // most recent best geusses at when the stick was pointed upwards
-double yAccelDiff[gyroHistorySize]; // elem X = yAccelHistory[X] - yAccelHistory[X-1]
-double accelWeights[gyroHistorySize];
+double yAccelDiff[historySize]; // elem X = yAccelHistory[X] - yAccelHistory[X-1]
+double accelWeights[historySize];
 double accelWeightTotal;
 unsigned long imuTimestamps[80];
 double gyroWeightTotal;
@@ -82,41 +82,15 @@ void initIMU() {
   Wire2.setSCL(I2C_SCL);
   Wire2.setClock(400000); //400khz clock - max for accelerometer?
 
-  for (int i = 0; i < 80; i++) {
-    upTimestamps[i] = (unsigned long) 0;
+  for (int i = 0; i < historySize; i++) {
+    gyroHistory[i] = (double) 0;
   }
-  for (int i = 0; i < 80; i++) {
-    downTimestamps[i] = (unsigned long) 0;
+  for (int i = 0; i < historySize; i++) {
+    accelHistory[i] = (double) 0;
   }
-  for (int i = 0; i < 80; i++) {
-    yAccelHistory[i] = (double) 0;
+  for (int i = 0; i < historySize; i++) {
+    timestamps[i] = (unsigned long) 0;
   }
-  for (int i = 0; i < 80; i++) {
-    yAccelDiff[i] = (double) 0;
-  }
-  for (int i = 0; i < 80; i++) {
-    imuTimestamps[i] = (double) 0;
-  }
-
-  for (int i = 0; i < 80; i ++) {
-    gyroZHistory[i] = (double) 0;
-  }
-
-}
-
-void updateGyro() {
-  unsigned long start = micros();
-  IMU.update();
-  unsigned long microdiff = micros() - start;
-  Serial.print("imu update micros: ");
-  Serial.println(microdiff);
-  IMU.getGyro(&gyroData);
-}
-
-double getRotationalSpeed() {
-  updateGyro();
-  // TODO: some function of gyroX/Y/Z?
-  return 0.0;
 }
 
 int loopCount = 0;
@@ -128,85 +102,75 @@ void stopRecording() {
   delay(1000);
 }
 
-double rotationSpeedEstimate() {
-  double sum = 0;
-  for (int i = 0; i < 100; i++) {
-    sum += (gyroZHistory[gyroPointer + i] * gyroWeights[i]);
+int boundedHistoryIndex(int anyIndex) {
+  int boundedIndex = anyIndex % historySize;
+  if (boundedIndex < 0) {
+    boundedIndex = boundedIndex + historySize;
   }
-  return sum / gyroWeightTotal;
+  return boundedIndex;
 }
 
-int unboundedGyroPointer(int unboundedIndex) {
-  return (unboundedIndex + gyroHistorySize) % gyroHistorySize;
-}
+double getGyro(int anyIndex) { return gyroHistory[boundedHistoryIndex(anyIndex)]; }
 
-int gyroAt(int unboundedIndex) {
-  return gyroZHistory[unboundedGyroPointer(unboundedIndex)];
-}
+double getCurGyro(int offset) { return getGyro(historyCurIndex + offset); }
 
-int gyroPointerFor(int offset) {
-  return (gyroHistoryPointer + gyroHistorySize + offset) % gyroHistorySize;
-}
+double getAccel(int anyIndex) { return accelHistory[boundedHistoryIndex(anyIndex)]; }
 
-double prevGyro(int offset) {
-  return gyroZHistory[gyroPointerFor(offset)];
-}
+double getCurAccel(int offset) { return getAccel(historyCurIndex + offset); }
+
+double getTimestamp(int anyIndex) { return timestamps[boundedHistoryIndex(anyIndex)]; }
+
+double getCurTimestamp(int offset) { return getTimestamp(historyCurIndex + offset); }
 
 unsigned long microsBetween(int startIndex, int endIndex) {
-  return timestamps[unboundedGyroPointer(endIndex)] - timestamps[unboundedGyroPointer(startIndex)];
+  return timestamps[boundedHistoryIndex(endIndex)] - timestamps[boundedHistoryIndex(startIndex)];
 }
 
 unsigned long timeSince(int offset) {
-  return timestamps[gyroHistoryPointer] - timestamps[gyroPointerFor(offset)];
+  return timestamps[historyCurIndex] - timestamps[getCurTimestamp(offset)];
 }
 
 void update() {
+  historyCurIndex = (historyCurIndex + 1) % historySize;
+
+  unsigned long startMicros = micros();
   IMU.update();
   IMU.getAccel(&accelData);
   IMU.getGyro(&gyroData);
-  gyroHistoryPointer = (gyroHistoryPointer + 1) % gyroHistorySize;
-  yAccelHistory[gyroHistoryPointer] = accelData.accY;
-  gyroZHistory[gyroHistoryPointer] = gyroData.gyroZ;
-  timestamps[gyroHistoryPointer] = micros();
+  unsigned long updateMicros = (micros() + startMicros) / 2;
+  timestamps[historyCurIndex] = updateMicros;
 
-  // TODO: accel at
-  double smoothedInit = (gyroAt(gyroHistoryPointer) * 0.6) + (gyroAt(gyroHistoryPointer - 1) * 0.15) + (gyroAt(gyroHistoryPointer - 2) * 0.05);
-  smoothedGyroHistory[gyroHistoryPointer] = smoothedInit;
-  smoothedGyroHistory[unboundedGyroPointer(gyroHistoryPointer - 1)] += gyroAt(gyroHistoryPointer) * 0.15;
-  smoothedGyroHistory[unboundedGyroPointer(gyroHistoryPointer - 2)] += gyroAt(gyroHistoryPointer) * 0.05;
+  accelHistory[historyCurIndex] = accelData.accY;
+  double gyroZX = pow(pow(gyroData.gyroZ, 2) + pow(gyroData.gyroX, 2), 0.5);
+  gyroHistory[historyCurIndex] = gyroZX;
 
-  // TODO: clear others at +3 as well?
-  smoothedGyroHistory[unboundedGyroPointer(gyroHistory + 3)] = 0;
+  // smoothedAccelHistory[i] will equal: weights [0.08, 0.12, 0.6, 0.12, 0.08] * raw accel values centered on curHistoryIndex
+  // smoothedAccelHistory[i] will only be complete once the curHistoryIndex = i + 2
+  double smoothedInit = (accelData.accY * 0.6) + (accelAt(historyCurIndex - 1) * 0.12) + (accelAt(historyCurIndex - 2) * 0.08);
+  smoothedAccelHistory[historyCurIndex] = smoothedInit;
+  smoothedAccelHistory[boundedHistoryIndex(historyCurIndex - 1)] += accelData.accY * 0.12;
+  smoothedAccelHistory[boundedHistoryIndex(historyCurIndex - 2)] += accelData.accY * 0.08;
+
+  gyroHistory[boundedHistoryIndex(historyCurIndex + 3)] = 0;
+  accelHistory[boundedHistoryIndex(historyCurIndex + 3)] = 0;
+  smoothedAccelHistory[boundedHistoryIndex(historyCurIndex + 3)] = 0;
 }
 
-bool isFastEnough() {
-  bool spinningFast = true
+bool isSpinning() {
+  bool spinningFast = true;
   for (int i = -5; i <= 0; i++) {
     spinningFast = spinningFast && (prevGyro(i) > minDegreesPerSec);
   }
   return spinningFast;
 }
 
-int boundedGyroIndex(int anyIndex) {
-  int boundedIndex = anyIndex % gyroHistorySize;
-  if (boundedIndex < 0) {
-    boundedIndex = boundedIndex + gyroHistorySize;
-  }
-  return boundedIndex;
-}
-
-// TODO: use everywhere
-double getGyroZ(int anyIndex) {
-  return gyroZHistory[boundedGyroIndex(anyIndex)];
-}
-
 double degreesTraveledForIndex(int start) {
-  double avgZ = (getGyroZ(start) + getGyroZ(start - 1)) / 2.0
-  return avgZ * microsBetween(start, start - 1)
+  double avgZ = (getGyro(start) + getGyro(start - 1)) / 2.0;
+  return avgZ * microsBetween(start, start - 1);
 }
 
 bool updateRevTracker() {
-  double degreesSinceLastTimestamp = gyroZHistory[gyroHistoryPointer] * timeSince(-1) / 1000000;
+  double degreesSinceLastTimestamp = gyroHistory[historyCurIndex] * timeSince(-1) / 1000000;
   // % 400 (instead of 360) to account for gyro integration not being perfect, to guarantee a high+low?
   double newAngle = (revTrackingAngle + degreesSinceLastTimestamp) % 360; 
   bool hasRevolved = newAngle < revTrackingAngle;
@@ -217,8 +181,8 @@ bool updateRevTracker() {
 int upIndexInLastRev() {
   double largestSmoothed = 0;
   int largestSmoothedIndex = revTrackingStartPointer;
-  for (int i = revTrackingStartPointer; i != gyroHistoryPointer - 2; i = (i + 1) % gyroHistorySize) {
-    double smoothed = abs(smoothedGyroHistory[i]);
+  for (int i = revTrackingStartPointer; i != historyCurIndex - 2; i = (i + 1) % historySize) {
+    double smoothed = abs(smoothedAccelHistory[i]);
     if (smoothed > largestSmoothed) {
       largestSmoothed = smoothed;
       largestSmoothedIndex = i;
@@ -231,7 +195,7 @@ int upIndexInLastRev() {
 double estimateAngleFromLastUp() {
   // obob?
   double estimate = 0;
-  for (int i = upIndex; i != gyroHistoryPointer + 1; i = (i + 1) % 60) {
+  for (int i = upIndex; i != historyCurIndex + 1; i = (i + 1) % 60) {
     estimate += degreesTraveledForIndex(i);
   }
   return estimate;
@@ -243,7 +207,7 @@ double updateAngleEstimate(bool revComplete) {
   bool notFirstRev = curAngleEstimate > -0.5;
   if (revComplete) {
     double estimatedFromUp = estimateAngleFromLastUp();
-    bool largeDiscrepancy = notFirstRev && (abs(curAngleEstimate - estimatedFromUp) > (50 + (successiveLargeDiscrepancies * 35)));
+    bool largeDiscrepancy = notFirstRev && (abs(curAngleEstimate - estimatedFromUp) > (50 + (successiveLargeDiscrepancies * 25)));
     if (largeDiscrepancy) {
       successiveLargeDiscrepancies += 1;
     } else {
@@ -253,27 +217,27 @@ double updateAngleEstimate(bool revComplete) {
       curAngleEstimate = estimatedFromUp;
     }
   } else if (notFirstRev) {
-    curAngleEstimate += degreesTraveledForIndex(gyroHistoryPointer);
+    curAngleEstimate += degreesTraveledForIndex(historyCurIndex);
     curAngleEstimate = curAngleEstimate % 360;
   }
 }
 
 void sample() {
   update();
-  if (!isFastEnough()) {
+  if (isSpinning()) {
+    bool revComplete = updateRevTracker();
+    if (revComplete) {
+      upIndex = smoothGyro();
+      newUpIndex = true;
+      upTimestamp = timestamps[upIndex];
+      revTrackingStartPointer = historyCurIndex;
+    }
+    updateAngleEstimate(revComplete);
+  } else {
     revTrackingAngle = 0;
-    revTrackingStartPointer = gyroHistoryPointer;
+    revTrackingStartPointer = historyCurIndex;
     curAngleEstimate = -1;
-    return
   }
-  bool revComplete = updateRevTracker();
-  if (revComplete) {
-    upIndex = smoothGyro();
-    newUpIndex = true;
-    upTimestamp = timestamps[upIndex];
-    revTrackingStartPointer = gyroHistoryPointer;
-  }
-  updateAngleEstimate(revComplete);
 }
 
 // TODO: only display image when stick has had a gyroZ > 400 in the last second
